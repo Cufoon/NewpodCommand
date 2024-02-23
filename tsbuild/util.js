ec7d4870acf1f0b95658ff39053ab847d3c0b35c
Mr. Lin <cufoon@qq.com>
2024-02-23 22:03:52 +0800

    fix: 一个domain拆分错误

--- tsbuild/util.js
+++ tsbuild/util.js
@@ -1,7 +1,7 @@
-import { ParseResultType, parseDomain } from 'parse-domain';
+import { ParseResultType, parseDomain, Validation } from 'parse-domain';
 import { getIsShowVerbose } from './store.js';
 export const getSplitDomain = (fullDomain) => {
-    const parseResult = parseDomain(fullDomain);
+    const parseResult = parseDomain(fullDomain, { validation: Validation.Lax });
     let mainDomain = fullDomain;
     let subDomain = '';
     if (parseResult.type === ParseResultType.Listed) {
